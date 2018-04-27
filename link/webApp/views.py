09da602c950abc5d07aeb152ebd04f837f2db7f3
Tjiho Lun <tom.darboux@eisti.fr>
2018-04-27 09:29:49 +0200

    check if url is valid

--- link/webApp/views.py
+++ link/webApp/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from webApp.forms import LinkForm
 from webApp.models import LinkModel
+import re
 # Create your views here.
 class Index(View):
     html = 'index.html'
@@ -19,10 +20,14 @@ class Index(View):
         if form.is_valid():
             print("valid !")
             link_data = form.cleaned_data['link']
-            try:
-                link_bdd = LinkModel.objects.create(link=link_data)
-            except:
-                error = "the link already exists"
+            p = re.compile('http(s)?:\/\/*')
+            if p.match(link_data):
+                try:
+                    link_bdd = LinkModel.objects.create(link=link_data)
+                except:
+                    error = "the link already exists"
+            else:
+                error = "not a valid url"
             
         list_link = LinkModel.objects.all()
         return render(request, self.html, locals())
